062e97864bec3eb5411685fd75da760118581e50
Tom Eyckmans <teyckmans@gmail.com>
2020-11-26 20:31:00 +0100

    added core module with tink dependency.

---
+++ core/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+}
+
+repositories {
+    // Use JCenter for resolving dependencies.
+    jcenter()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("com.google.crypto.tink:tink:1.5.0")
+
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,4 +8,5 @@
  */
 
 rootProject.name = "gradle-tink"
+include("core")
 include("plugin")
